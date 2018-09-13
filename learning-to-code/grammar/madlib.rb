1457304be26d3b6f1be0ad606c477811a7c99f91
rae <kelsey.shrauner@gmail.com>
2018-09-13 15:43:33 -0700

    this is the madlib i created
    if i want to run it again, i can paste it into repl

---
+++ learning-to-code/grammar/madlib.rb
@@ -0,0 +1,36 @@
+
+print "pick a number between 1 and 5:"
+number_of_days = gets.chomp
+
+print "an activity you do on the sidewalk ending in -ing?"
+ing_activity = gets.chomp
+
+print "sassiest article of clothing you can think of, real or imaginary:"
+clothing_article = gets.chomp
+
+print "what's something annoying men say to you on the street?"
+annoying_saying = gets.chomp
+
+print "what's your favorite ninja move?"
+ninja_move = gets.chomp
+
+print "name a creature you can ride"
+rideable_creature = gets.chomp
+
+print "an obnoxious petname:"
+pet_name = gets.chomp
+
+
+
+
+puts number_of_days +" days ago i was" 
+puts ing_activity + " on the"
+puts "sidewalk, wearing" 
+puts " my best " + clothing_article + ","
+puts " when a man approached me" 
+puts " out of nowhere and" 
+puts " said " + annoying_saying+ "."
+puts "I performed a " +ninja_move
+puts "to send him a message,"
+puts "hopped on my " + rideable_creature +"," 
+puts "and shouted, later, " + pet_name +"!"
